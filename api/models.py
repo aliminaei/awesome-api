8975bd3516087a33019e17e368251b7d706325cb
Ali Minaei <ali.minaei@gmail.com>
2016-09-03 20:15:49 +0100

    adding the API_User table

--- api/models.py
+++ api/models.py
@@ -2,4 +2,9 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class API_User(models.Model):
+    username             = models.TextField()
+    first_name           = models.TextField()
+    last_name            = models.TextField()
+    email                = models.TextField()
+    password             = models.TextField()
