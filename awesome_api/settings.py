a9935eb8b8f93d7f95699fdfedd62a9a1709dfed
Ali Minaei <ali.minaei@gmail.com>
2016-09-04 16:59:14 +0100

    adding static files

--- awesome_api/settings.py
+++ awesome_api/settings.py
@@ -122,4 +122,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
