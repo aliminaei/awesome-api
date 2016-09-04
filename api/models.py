8cd5fbd8651512e722bbdffbc54a5be902d66c1a
Ali Minaei <ali.minaei@gmail.com>
2016-09-04 21:35:12 +0100

    making the username unique

--- api/models.py
+++ api/models.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 class API_User(models.Model):
-    username             = models.TextField()
+    username             = models.TextField(unique=True)
     first_name           = models.TextField()
     last_name            = models.TextField()
     email                = models.TextField()
