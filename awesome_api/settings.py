89e90e97aa81c48e26d8deafd839eab2ca36b750
Ali Minaei <ali.minaei@gmail.com>
2016-09-04 16:41:06 +0100

    static files again

--- awesome_api/settings.py
+++ awesome_api/settings.py
@@ -124,9 +124,3 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-    os.path.join(PROJECT_ROOT, 'staticfiles'),
-)
